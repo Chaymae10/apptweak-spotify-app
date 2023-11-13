dfeac2a79ec71a5d8aab418710f34eab982c4f3a
Chaymae Hbat <hbat.chaymae2@gmail.com>
2023-11-13 19:00:55 +0100

    modifie select component to add autocompilation

--- src/Components/PlaylistComponent/PlaylistComponent.tsx
+++ src/Components/PlaylistComponent/PlaylistComponent.tsx
@@ -1,9 +1,7 @@
 import React, { FC, ReactElement, useState, useEffect } from "react";
 import { getPlaylistDetailsRequest, getPlaylistTracksRequest, setSelectedPlaylist } from "../../containers/actions/actions";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import "./PlaylistComponent.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { authSelectors } from "../../containers/auth/selectors";
@@ -13,44 +11,38 @@ const PlaylistList: FC = (): ReactElement => {
   const dispatch = useDispatch();
   const playlists = useSelector(authSelectors.getPlaylists);
 
-  //Function to handle playlist selection change
-  const handlePlaylistSelectionChange  = async (event: any) => {
-      // Extract the playlist ID from the selected value in the event
-    const playlistId = event.target.value as string;
-    setSelectedPlaylistId(playlistId);
+  const handlePlaylistSelectionChange = async (event: any, value: any) => {
+    if (value) {
+      const playlistId = value.id;
+      setSelectedPlaylistId(playlistId);
   
-    // Search for the playlist in the array
-    const selectedPlaylist = playlists.items.find((playlist) => playlist.id === playlistId);
+      const selectedPlaylist = playlists.items.find((playlist) => playlist.id === playlistId);
   
-    // Vérification si la playlist a été trouvée
-    if (selectedPlaylist) {
-      //Use the setSelectedPlaylist action to update the global state of selected playlist
-      dispatch(setSelectedPlaylist(selectedPlaylist));
-
-      dispatch(getPlaylistDetailsRequest({ playlistId }));
-      dispatch(getPlaylistTracksRequest({ playlistId }));
+      if (selectedPlaylist) {
+        dispatch(setSelectedPlaylist(selectedPlaylist));
+        dispatch(getPlaylistDetailsRequest({ playlistId }));
+        dispatch(getPlaylistTracksRequest({ playlistId }));
+      }
     }
   };
-  
-  
 
   return (
     <div className="playlist-container">
-      <FormControl color="success">
-        <InputLabel>Select a playlist</InputLabel>
-        <Select
-          className="select"
-          onChange={handlePlaylistSelectionChange}
-          label="Select a playlist"
-          value={selectedPlaylistId}
-        >
-          {playlists.items.map((playlist) => (
-            <MenuItem key={playlist.id} value={playlist.id}>
-              {playlist.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <Autocomplete
+        value={playlists.items.find(playlist => playlist.id === selectedPlaylistId) || null}
+        options={playlists.items}
+        getOptionLabel={(option) => option.name}
+        renderInput={(params) => (
+          <TextField color="success"
+            {...params}
+            label="Select a playlist"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+        onChange={handlePlaylistSelectionChange}
+        className="select"
+      />
     </div>
   );
 };
